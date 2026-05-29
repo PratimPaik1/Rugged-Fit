e658ac003535d806c9dd1dc9c455b8115677709f
PratimPaik1 <PratimPaik0@gmail.com>
2026-05-29 19:23:30 +0530

    cors erroe fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -30,6 +30,8 @@ app.use(express.json());
 //     credentials: true
 // }));
 
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
+
 // Serve static
 app.use(express.static(path.join(__dirname, "..", "public")));
 
